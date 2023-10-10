5088e5479224dc578a98e9e1791c64f5fecf2147
Afrizal Amri Fahmi <106945335+afrizalamri@users.noreply.github.com>
2023-10-10 21:38:00 +0800

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://vercel.com/afrizalamri/webgis-frasta/public/';
+    public string $baseURL = 'https://vercel.com/afrizalamri';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
